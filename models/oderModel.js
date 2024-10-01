714fffded2b217670b9b60801a15de85fca0d3e1
irfan_usuf <irfanusuf33@gmail.com>
2024-10-01 17:14:08 +0530

    fixes

--- models/oderModel.js
+++ models/oderModel.js
@@ -14,8 +14,8 @@ const orderSchema = new mongoose.Schema({
     totalAmount: { type: Number, required: true }, 
     orderDate: { type: Date, default: Date.now },  
     shippingTime: { type: Date, default: () => new Date(new Date().setDate(new Date().getDate() + 7)) },
-    emailVerified : Boolean,
-    isPaymentDone : Boolean,
+    emailVerified : {type : Boolean , default : false},
+    isPaymentDone : {type : Boolean , default : false},
     status: { type: String, default: 'pending' } , 
   });
   
