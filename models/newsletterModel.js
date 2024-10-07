cc624be5d8541b4fad842651c2c3b6bd58ebed24
irfan_usuf <irfanusuf33@gmail.com>
2024-10-07 16:13:25 +0530

    addedSvg

---
+++ models/newsletterModel.js
@@ -0,0 +1,17 @@
+
+
+
+const mongoose = require("mongoose");
+
+const NewsLetter = mongoose.model("NewsLetter" , {
+
+
+    email : {type : String , require : true}
+
+
+
+
+})
+
+module.exports  =  NewsLetter
+
